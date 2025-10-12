4465eb2a313fbee59b5dfbfa3a082a6c9b661702
ssimofatine <simohaji147@gmail.com>
2025-10-12 16:56:42 +0200

    Ejercicio1

--- EjerciciosPython1/Ejercicio4.py
+++ EjerciciosPython1/Ejercicio4.py
@@ -2,7 +2,7 @@
 # Pide al usuario una palabra y muestra cuántas vocales contiene.
 
 # aqui pide al usuario una palabra
-balabra = input("dime tú palabra: ")
+palabra = input("dime tú palabra: ")
 
 # aqui almacinar total del vocales
 contador = 0
@@ -12,11 +12,11 @@ laletra = "aieouAIEOUáíéóúÁÉÍÓÚ"
 
 # ahora mejor usa metodo de ( FOR con IF )
 
-for i in balabra:
+for i in palabra:
     if i in laletra:
         contador = contador + 1
 
-print(f"La palabra '{balabra}' tiene {contador} vocales.")
+print(f"La palabra '{palabra}' tiene {contador} vocales.")
 
 
 
