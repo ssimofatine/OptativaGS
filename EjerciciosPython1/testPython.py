9694a49933e71a0276131dec8e6390e8ba381e8e
ssimofatine <simohaji147@gmail.com>
2025-11-08 18:46:11 +0100

    Ejercicio1

--- EjerciciosPython1/testPython.py
+++ EjerciciosPython1/testPython.py
@@ -1,4 +1,24 @@
-simo = input("help")
-c = float(simo)
+# for con range
+for i in range(5):
+    print(i)
 
-print(c)
+print("----------------")
+
+for i in range(1, (20+1)):
+    print(i)
+
+print("----------------")
+language = 'Python'
+for letter in language:
+    print(letter)
+
+print("----------------")
+
+for i in range(len(language)):
+    print(language[i])
+
+print("----------------")
+contador = 7
+if contador > 4 and contador < 11:
+    print(f"--------   {contador}   --------")
+
