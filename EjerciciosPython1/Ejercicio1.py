d017f8ea4d26aee52c7ce688309874bb0a286676
ssimofatine <simohaji147@gmail.com>
2025-11-09 17:15:47 +0100

    Ejercicio1

--- EjerciciosPython1/Ejercicio1.py
+++ EjerciciosPython1/Ejercicio1.py
@@ -3,10 +3,27 @@
 # Escribe un programa que convierta grados Celsius a Fahrenheit.
 # Fórmula: ( F = C x 9/5 + 32 ).
 
-c1 = input("por favor Dime grado celsius: ")
+def convierta(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+
+
+numBoolean = True
+while numBoolean:
+    c1 = input("por favor Dime grado celsius: ")
+    if convierta(c1):
+        break
+    else:
+        print("tu numero no es numero entere porfa introduce comó este Ej(1, 2, 3, 55.4)")
+
+
 
 c = float(c1)
 
 F = (c * 9 / 5) + 32
-
-print(F)
+print("--------------------")
+print(f"{F} F")
+print("Adios")
