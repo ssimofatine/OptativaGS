f42395de515921f11da114419373c6c44b3cd1f1
ssimofatine <simohaji147@gmail.com>
2025-11-26 01:12:10 +0100

    Ejercicio1

---
+++ EjerciciosPython1/Ejercicio5.py
@@ -0,0 +1,26 @@
+lista_caja = []
+
+numero_uno = input("pon numero 1: ")
+lista_caja.append(numero_uno)
+
+numero_dos = input("pon numero 2: ")
+lista_caja.append(numero_dos)
+
+numero_tres = input("pon numero 3: ")
+lista_caja.append(numero_tres)
+
+numero_cuatro = input("pon numero 4: ")
+lista_caja.append(numero_cuatro)
+
+numero_cinco = input("pon numero 5: ")
+lista_caja.append(numero_cinco)
+
+lista_otra = []
+
+lista_otra.append(lista_caja[4])
+lista_otra.append(lista_caja[3])
+lista_otra.append(lista_caja[2])
+lista_otra.append(lista_caja[1])
+lista_otra.append(lista_caja[0])
+
+print(lista_otra)
